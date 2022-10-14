28eb9cf961ba58c0eab3631c48177e67888fa618
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-10-13 21:06:54 -0700

    Update string variables and add puts statements

--- variables.rb
+++ variables.rb
@@ -2,6 +2,12 @@ pet_name_1 = "Maddie"
 pet_name_2 = "Daisy"
 daughter_name = "Hailey"
 
+# Reassigned String Variables
+
+pet_name_1 = "Thelma"
+pet_name_2 = "Louise"
+daughter_name = "Harper"
+
 pet_age_1 = 2
 pet_age_2 = 2
 husgand_age = 48
@@ -11,3 +17,9 @@ diaper_cost = 0.25
 
 tired = true
 tall = false
+
+puts "#{pet_name_1} and #{pet_name_2} are always up to no good!"
+
+puts "I can't believe #{daughter_name} is #{current_daughter_age} years old."
+
+puts "My husband asked, aren't you tired? I said, #{tired}."
